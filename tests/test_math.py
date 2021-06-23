7f7747d8d5d27cd90b91452b96d315423996f4d1
Rodney Gauna <rodneygauna@gmail.com>
2021-06-22 22:04:23 -0700

    first failing test

--- tests/test_math.py
+++ tests/test_math.py
@@ -11,3 +11,10 @@ Their purpose is to show how to use the pytest framework by example.
 def test_one_plus_one():
     """This test will pass."""
     assert 1 + 1 == 2
+
+def test_one_plus_two():
+    """This test will fail."""
+    a = 1
+    b = 2
+    c = 0
+    assert a + b == c
