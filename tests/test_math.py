260e0c21585bfa52cdf101b40bdc71e751bb8164
Rodney Gauna <rodneygauna@gmail.com>
2021-06-22 22:03:57 -0700

    first passing test

---
+++ tests/test_math.py
@@ -0,0 +1,13 @@
+"""
+This module contains basic unit tests for math operations.
+Their purpose is to show how to use the pytest framework by example.
+"""
+
+# ------------------------------------------------------------------------------
+# A most basic test fuction
+# ------------------------------------------------------------------------------
+
+
+def test_one_plus_one():
+    """This test will pass."""
+    assert 1 + 1 == 2
