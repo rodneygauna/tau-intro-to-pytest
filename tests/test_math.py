f6ba416bd370dabf37e7c6d677064b5de817ff16
Rodney Gauna <rodneygauna@gmail.com>
2021-06-22 22:18:44 -0700

    using pytest to handle exceptions

--- tests/test_math.py
+++ tests/test_math.py
@@ -4,17 +4,37 @@ Their purpose is to show how to use the pytest framework by example.
 """
 
 # ------------------------------------------------------------------------------
-# A most basic test fuction
+# Imports
 # ------------------------------------------------------------------------------
 
+import pytest
+
+
+# ------------------------------------------------------------------------------
+# A basic test case that will pass
+# ------------------------------------------------------------------------------
 
 def test_one_plus_one():
-    """This test will pass."""
     assert 1 + 1 == 2
 
+
+# ------------------------------------------------------------------------------
+# A basic test case that will fail
+# ------------------------------------------------------------------------------
+
 def test_one_plus_two():
-    """This test will fail."""
     a = 1
     b = 2
     c = 0
     assert a + b == c
+
+
+# ------------------------------------------------------------------------------
+# A test function that verifies an exception
+# ------------------------------------------------------------------------------
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError) as e:
+        num = 1 / 0
+
+    assert 'division by zero' in str(e.value)
